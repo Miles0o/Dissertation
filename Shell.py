963b7ff0678b409732b8d46e47a4aa60a900b02a
Miles0o <52213928+Miles0o@users.noreply.github.com>
2021-04-14 19:41:16 +0100

    objectified symspell
    oop'ed

--- Shell.py
+++ Shell.py
@@ -1,4 +1,5 @@
 from Bing_API import API_talker
 API = API_talker.API_talker()
 
-print(API.API_call("so if i just type somthing out will you just work?"))
+
+#print(API.API_call("so if i just type somthing out will you just work?"))
--- local_spellchecker/pySpell.py
+++ local_spellchecker/pySpell.py
@@ -1,19 +1,25 @@
 import time
 from symspellpy import SymSpell, Verbosity
 
-sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
-dictionary_path = "D:\github\Dissertation\\venv\Lib\site-packages\symspellpy\\frequency_dictionary_en_82_765.txt"
-# term_index is the column of the term and count_index is the
-# column of the term frequency
-sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+class pySpell:
 
-# lookup suggestions for single-word input strings
-input_term = "hollo, welcume"  # misspelling of "members"
-# max edit distance per lookup
-# (max_edit_distance_lookup <= max_dictionary_edit_distance)
-suggestions = sym_spell.lookup(input_term, Verbosity.CLOSEST,
-                               max_edit_distance=2)
-# display suggestion term, term frequency, and edit distance
+    def __init__(self):
+        #innitilisation
+        self.sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+        self.dictionary_path = "D:\github\Dissertation\\venv\Lib\site-packages\symspellpy\\frequency_dictionary_en_82_765.txt"
+        # term_index is the column of the term and count_index is the
+        # column of the term frequency
+        self.sym_spell.load_dictionary(self.dictionary_path, term_index=0, count_index=1)
 
-for suggestion in suggestions:
-    print(suggestion)
+    # lookup suggestions for single-word input strings
+    def lookup(self, word):
+        # max edit distance per lookup
+        # (max_edit_distance_lookup <= max_dictionary_edit_distance)
+        suggestions = self.sym_spell.lookup(word, Verbosity.CLOSEST,
+                                   max_edit_distance=3)
+        return suggestions
+
+    def correctPhrase(self, sentance):
+        #TODO: AJFLA
+        next()
+
