e048991ddf4cfe5dcbb0b06bd5a9311a40195498
Miles0o <52213928+Miles0o@users.noreply.github.com>
2021-04-15 12:20:54 +0100

    testing symspell
    some testing to find understanding

--- Shell.py
+++ Shell.py
@@ -3,6 +3,6 @@ from Bing_API import API_talker
 API = API_talker.API_talker()
 speller = pySpell.pySpell()
 
-for word in speller.correctPhrase('hollo worlb'):
+for word in speller.lookup('loanly'):
     print(word)
 #print(API.API_call("so if i just type somthing out will you just work?"))
--- local_spellchecker/pySpell.py
+++ local_spellchecker/pySpell.py
@@ -5,7 +5,9 @@ class pySpell:
 
     def __init__(self):
         #innitilisation
-        self.sym_spell = SymSpell(max_dictionary_edit_distance=8, prefix_length=9)
+        #lowwer prefix number seems to have quicker load times
+        #max edit distance affecs the max distance allowed, its kinda dumb ngl
+        self.sym_spell = SymSpell(max_dictionary_edit_distance=6, prefix_length=7)
         self.dictionary_path = "D:\github\Dissertation\\venv\Lib\site-packages\symspellpy\\frequency_dictionary_en_82_765.txt"
         # term_index is the column of the term and count_index is the
         # column of the term frequency
@@ -15,12 +17,12 @@ class pySpell:
     def lookup(self, word):
         # max edit distance per lookup
         # (max_edit_distance_lookup <= max_dictionary_edit_distance)
-        suggestions = self.sym_spell.lookup(word, Verbosity.CLOSEST, max_edit_distance=2)
+        suggestions = self.sym_spell.lookup(word, Verbosity.CLOSEST, max_edit_distance=3)
         return suggestions
 
     def correctPhrase(self, sentance):
         # max edit distance per lookup
         # (max_edit_distance_lookup <= max_dictionary_edit_distance)
-        suggestions = self.sym_spell.lookup_compound(sentance, max_edit_distance=8)
+        suggestions = self.sym_spell.lookup_compound(sentance, max_edit_distance=6)
         return suggestions
 
