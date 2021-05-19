95d997d50564da49ac024145bed1e100c11d37c9
Miles0o <52213928+Miles0o@users.noreply.github.com>
2021-05-19 22:12:30 +0100

    results
    gramorly results compleat, started programming Bing script to get results

--- Bing_API/check.py
+++ Bing_API/check.py
@@ -1,14 +1,31 @@
 import json
 
 f = open('D:\github\Dissertation\Bing_API\Bing_response.json')
-check = open('D:\github\Dissertation\data\\treated\small 6.txt', 'r', errors='ignore')
-
-lines = check.readlines()
 responses = json.load(f)
+files = []
+file_names = ["large 1:", "medium 1:", "medium 2:", "medium 3:", "small 1:", "small 2:", "small 3:", "small 4:", "small 5:", "small 6:", "small 7:"]
+files.append(open('D:\github\Dissertation\data\\treated\large 1.txt.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\medium 1.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\medium 2.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\medium 3.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\small 1.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\small 2.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\small 3.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\small 4.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\small 5.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\small 6.txt', 'r', errors='ignore'))
+files.append(open('D:\github\Dissertation\data\\treated\small 7.txt', 'r', errors='ignore'))
 
-print(len(responses))
+index = 0
+for file in files:
+    lines = file.readlines()
+    write = open('data/Results/Bing API.txt', 'w')
+    write.write(file_names[index])
+    result = []
+    for line in lines:
 
-for line in lines:
-    if line.split(" ")[0] not in responses:
-        print(line.split(" ")[0])
 
+
+
+    write.writelines(result)
+    index += 1
